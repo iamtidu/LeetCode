527f908a7543969f40fb23daa0a2384814d527b0
UDIT KUMAR <117339836+iamtidu@users.noreply.github.com>
2024-04-13 22:03:48 +0530

    Add files via upload

---
+++ 14. Longest Common Prefix/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        
+        String prefix = "";
+       
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            
+            for (int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
+                    
+                    return prefix;
+                }
+            }
+           
+            prefix += c;
+        }
+        
+        return prefix;
+    }
+}
